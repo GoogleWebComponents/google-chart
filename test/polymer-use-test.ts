60e325d4ce039f8bf2b8b4caa6b1740a8af6a7c5
Rafał Sławik <rslawik@google.com>
2020-07-10 15:01:27 +0100

    Remove selection-changed event (#295)
    The selection-changed event is Polymer-specific. It is used for two-way
    bindings and when the selection property changes. There is also the
    google-chart-select event fired when the user selects something. This
    makes the former event redundant.

--- test/polymer-use-test.ts
+++ test/polymer-use-test.ts
@@ -18,6 +18,7 @@
 import '../google-chart.js';
 import {customElement, property} from '@polymer/decorators';
 import {PolymerElement, html} from '@polymer/polymer';
+import {GoogleChart} from '../google-chart.js';
 import {DataTableLike} from '../loader.js';
 import {ready} from './helpers.js';
 
@@ -25,20 +26,24 @@ const assert = chai.assert;
 
 suite('<google-chart> use in Polymer element', () => {
   let element: GoogleChartTestElement;
+
   setup(async () => {
     element = new GoogleChartTestElement();
     document.body.append(element);
     await ready(element);
   });
+
   teardown(() => {
     element?.remove();
   })
+
   test('passes properties', () => {
     const chartDiv =
         element.$['chart'].shadowRoot!.getElementById('chartdiv')!;
     assert.include(chartDiv.innerText, 'Value');
     assert.include(chartDiv.innerText, 'Something');
   });
+
   test('deep options change via binding', async () => {
     element.set('options.title', 'New title');
     const chartDiv =
@@ -46,13 +51,33 @@ suite('<google-chart> use in Polymer element', () => {
     await ready(element);
     assert.include(chartDiv.innerText, 'New title');
   });
+
+  test('two-way binding', async () => {
+    // chart-selection-changed fires because the propery has {notify: true}.
+    const chartSelectionChanged = new Promise(resolve => {
+      element.addEventListener('chart-selection-changed', resolve, {once: true});
+    });
+    // Get chartWrapper and simulate user selection:
+    // https://developers.google.com/chart/interactive/docs/dev/events#firing-an-event
+    const chartWrapper: google.visualization.ChartWrapper=
+        (element.$['chart'] as GoogleChart)['chartWrapper']!;
+    chartWrapper.getChart().setSelection([{row: 1}]);
+    google.visualization.events.trigger(chartWrapper.getChart(), 'select', {});
+    await chartSelectionChanged;
+
+    assert.sameDeepMembers(element.chartSelection!, [{row: 1, column: null}]);
+  });
 });
 
 @customElement('google-chart-polymer-test')
 class GoogleChartTestElement extends PolymerElement {
   static get template() {
     return html`
-      <google-chart id="chart" options="[[options]]" data="[[data]]">
+      <google-chart
+          id="chart"
+          options="[[options]]"
+          data="[[data]]"
+          selection="{{chartSelection::google-chart-select}}">
       </google-chart>
     `;
   }
@@ -64,5 +89,10 @@ class GoogleChartTestElement extends PolymerElement {
   data: DataTableLike = [
     ['Data', 'Value'],
     ['Something', 1],
+    ['Thing', 2],
+    ['Entry', 3],
   ];
+
+  @property({type: Array, notify: true})
+  chartSelection: unknown[]|undefined;
 }
